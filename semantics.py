d40182e5aca15bf185db64414129655323f870b8
Gertf <gertfou@gmail.com>
2023-06-28 13:42:02 +0700

    1st commit

---
+++ semantics.py
@@ -0,0 +1,35 @@
+import spacy 
+
+nlp = spacy.load('en_core_web_md')
+
+word1 = nlp("cat")
+word2 = nlp("monkey")
+word3 = nlp("banana")
+
+print(word1.similarity(word2))
+print(word3.similarity(word2))
+print(word3.similarity(word1))
+
+# the results are printed as integers or %, the similarity makes sense based on value
+
+# vectors
+
+tokens = nlp('human alien shoe sock ')
+for token1 in tokens:
+    for token2 in tokens:
+        print(token1.text, token2.text, token1.similarity(token2))
+
+# working with sentences
+
+sentence_to_compare = "Why is my cat on the car"
+
+sentences = ["where did my dog go",
+"Hello, there is my car",
+"I\'ve lost my car in my car",
+"I\'d like my boat back",
+"I will name my dog Diana"]
+
+model_sentence = nlp(sentence_to_compare)
+for sentence in sentences:
+    similarity = nlp(sentence).similarity(model_sentence)
+    print(sentence + " - ", similarity)
